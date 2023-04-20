debe21446d7ea4fc4225fcbae3d6f7ff2cb88dc6
nfukuma <100755561+NaoyaFukuma@users.noreply.github.com>
2023-04-20 17:08:33 +0900

    listenソケットを扱うクラスを作成

---
+++ srcs/socket/listen.cpp
@@ -0,0 +1,40 @@
+#include "listen.hpp"
+#include "../conf/conf.hpp"
+#include <netinet/in.h>
+#include <stdexcept>
+#include <string>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <vector>
+
+// コンストラクタ
+Listen::Listen(const std::vector<in_port_t> &ports)
+    : port_num_(ports.size()), fds_(port_num_) {
+  for (int i = 0; i < port_num_; i++) {
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd == -1) {
+      throw std::runtime_error("socket() failed");
+    }
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = ports[i];
+    addr.sin_addr.s_addr = INADDR_ANY;
+    if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+      throw std::runtime_error("bind() failed");
+    }
+    if (listen(fd, SOMAXCONN) == -1) {
+      throw std::runtime_error("listen() failed");
+    }
+    fds_[i] = fd;
+  }
+}
+
+/* テストmain */
+int main() {
+  std::vector<in_port_t> ports;
+  ports.push_back(htons(80));
+  ports.push_back(htons(8080));
+  Listen listen(ports);
+  return 0;
+}
---
+++ srcs/socket/listen.hpp
@@ -0,0 +1,30 @@
+#ifndef LISTEN_HPP
+#define LISTEN_HPP
+
+#include "../conf/conf.hpp"
+#include <string>
+#include <vector>
+
+/* webserverにおける、リッスンソケットを扱うクラス */
+class Listen {
+private:
+  int port_num_;         // ポートの数
+  std::vector<int> fds_; // リッスンソケットのファイルディスクリプタ
+  Listen(const Listen &other); // コピーコンストラクタは不使用(シングルトン)
+  void operator=(const Listen &other); // コピー代入演算子も不使用(シングルトン)
+  Listen(); // デフォルトコンストラクタは不使用
+public:
+  Listen(const std::vector<in_port_t> &ports); // コンストラクタ
+  ~Listen(){};                                 // デストラクタ
+  bool operator==(const int fd) const {
+    for (int i = 0; i < this->port_num_; i++) {
+      if (this->fds_[i] == fd) {
+        return true;
+      }
+    }
+    return false;
+  } // 比較演算子
+    // 新たな接続要求がクライアントからあったかどうかを判定するために使用
+};
+
+#endif
