4139082a26c39853be0f585a0c1662635167d52e
nfukuma <100755561+NaoyaFukuma@users.noreply.github.com>
2023-04-24 13:31:58 +0900

    コンプファイルのパースのドラフト作成

--- srcs/conf/conf.cpp
+++ srcs/conf/conf.cpp
@@ -1,10 +1,307 @@
 #include "conf.hpp"
-
-// .confファイルが指定されなかった場合に使用し、デフォルトの設定を行う
-conf::conf() {}
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
 
 // .confファイルが指定された場合に使用し、設定ファイルを読み込む
-conf::conf(const std::string &conf_path) {}
+Conf::Conf(const char *conf_path) {
+  // open
+  std::ifstream ifs(conf_path);
+  if (!ifs) {
+    std::cerr << "conf file not found" << std::endl;
+    throw std::invalid_argument("conf file not found");
+  }
+
+  // parse
+  std::string line;
+  while (std::getline(ifs, line)) {
+    if (line.empty()) {
+      continue;
+    }
+    if (line[0] == '#') {
+      continue;
+    }
+    if (line.find("server ") != std::string::npos) {
+      this->server_vec_.push_back(Server());
+      Conf::parse_sever(ifs, this->server_vec_.back());
+    } else {
+      std::cout << line << std::endl;
+      std::cerr << "invalid conf file 30" << std::endl;
+      throw std::invalid_argument("invalid conf file");
+    }
+  }
+}
+
+int inet_addr(std::string ip) {
+  int res = 0;
+  int tmp = 0;
+  int cnt = 0;
+
+  while (ip.find(".") != std::string::npos) {
+    cnt++;
+    if (cnt > 3) {
+      std::cerr << "invalid ip address" << std::endl;
+      throw std::invalid_argument("invalid ip address");
+    }
+    tmp = atoi(ip.substr(0, ip.find(".")).c_str());
+    if (tmp > 255 || tmp < 0) {
+      std::cerr << "invalid ip address" << std::endl;
+      throw std::invalid_argument("invalid ip address");
+    }
+    res = (res << 8) + tmp;
+    ip = ip.substr(ip.find(".") + 1);
+  }
+  tmp = atoi(ip.c_str());
+  res = (res << 8) + tmp;
+  return res;
+}
+
+void Conf::parse_sever(std::ifstream &ifs, Server &server) {
+  std::string line;
+  while (std::getline(ifs, line)) {
+    if (line.empty()) {
+      continue;
+    }
+    if (line[0] == '#') {
+      continue;
+    }
+    // listen, server_name, location, location_back, }のいずれかが来る
+
+    if (line.find("listen ") != std::string::npos) {
+      size_t pos = line.find("listen ");
+      std::string addr = line.substr(pos + 7);
+      if (addr.find(":") != std::string::npos) {
+        server.listen_.sin_addr.s_addr =
+            htons(inet_addr(addr.substr(0, addr.find(":"))));
+        server.listen_.sin_port =
+            htons(atoi(addr.substr(addr.find(":") + 1).c_str()));
+      } else {
+        server.listen_.sin_addr.s_addr = INADDR_ANY;
+        server.listen_.sin_port = htons(atoi(addr.c_str()));
+      }
+
+    } else if (line.find("server_name ") != std::string::npos) {
+      std::string server_names = line.substr(line.find("server_name ") + 12);
+      std::vector<std::string> sv_names_vec;
+      while (server_names.length()) {
+        if (server_names.find(" ") != std::string::npos) {
+          sv_names_vec.push_back(
+              server_names.substr(0, server_names.find(" ")));
+          server_names = server_names.substr(server_names.find(" ") + 1);
+        } else {
+          sv_names_vec.push_back(
+              server_names.substr(0, server_names.find(";")));
+          server_names = server_names.substr(server_names.find(";") + 1);
+        }
+      }
+      server.sv_names_ = sv_names_vec;
+
+    } else if (line.find("location ") != std::string::npos) {
+      server.locations_.push_back(Location());
+      server.locations_.back().match_ = prefix;
+      Conf::parse_location(ifs, server.locations_.back());
+
+    } else if (line.find("location_back ") != std::string::npos) {
+      server.locations_.push_back(Location());
+      server.locations_.back().match_ = back;
+      Conf::parse_location(ifs, server.locations_.back());
+
+    } else if (line.find("}") != std::string::npos) {
+      break;
+
+    } else {
+      std::cerr << "invalid conf file 102" << std::endl;
+      throw std::invalid_argument("invalid conf file");
+    }
+  }
+}
+
+void Conf::parse_location(std::ifstream &ifs, Location &location) {
+  location.is_cgi_ = false;
+  location.autoindex_ = false;
+  location.client_max_body_size_ = 1 * 1024 * 1024; // デフォルトは1MB
+  std::string line;
+  while (std::getline(ifs, line)) {
+    if (line.empty()) {
+      continue;
+    }
+    if (line[0] == '#') {
+      continue;
+    }
+    // allow_ method, client_max_body_size, root, index, is_cgi, cgi_executor,
+    // error_page, autoindex, return, }のいずれかが来る
+
+    if (line.find("allow_method ") != std::string::npos) {
+      std::string method = line.substr(line.find("allow_method ") + 13);
+      while (method.length()) {
+        std::string tmp;
+        if (method.find(" ") != std::string::npos) {
+          tmp = method.substr(0, method.find(" "));
+          method = method.substr(method.find(" ") + 1);
+        } else {
+          tmp = method.substr(0, method.find(";"));
+          method = method.substr(method.find(";") + 1);
+        }
+        if (tmp == "GET") {
+          location.allow_method_ += GET;
+        } else if (tmp == "POST") {
+          location.allow_method_ += POST;
+        } else if (tmp == "DELETE") {
+          location.allow_method_ += DELETE;
+        } else {
+          std::cerr << "invalid conf file 133" << std::endl;
+          throw std::invalid_argument("invalid conf file");
+        }
+      }
+
+    } else if (line.find("client_max_body_size ") != std::string::npos) {
+      std::string size_str =
+          line.substr(line.find("client_max_body_size ") + 21);
+      size_str.pop_back();
+      size_t multiplier = 1;
+      size_t value = 0;
+
+      char unit = size_str.back();
+      if (unit == 'k' || unit == 'K') {
+        multiplier = 1024;
+        size_str.pop_back();
+      } else if (unit == 'm' || unit == 'M') {
+        multiplier = 1024 * 1024;
+        size_str.pop_back();
+      } else if (unit == 'g' || unit == 'G') {
+        multiplier = 1024 * 1024 * 1024;
+        size_str.pop_back();
+      }
+
+      try {
+        value = std::stoull(size_str);
+      } catch (const std::invalid_argument &e) {
+        throw std::runtime_error("Invalid client_max_body_size format");
+      } catch (const std::out_of_range &e) {
+        throw std::runtime_error("Value out of range");
+      }
+      location.client_max_body_size_ = value * multiplier;
+
+    } else if (line.find("root ") != std::string::npos) {
+      location.root_ = line.substr(line.find("root ") + 5);
+      location.root_.pop_back();
+
+    } else if (line.find("index ") != std::string::npos) {
+      location.index_ = line.substr(line.find("index ") + 6);
+      location.index_.pop_back();
+
+    } else if (line.find("is_cgi ") != std::string::npos) {
+      if (line.find("on") != std::string::npos) {
+        location.is_cgi_ = true;
+      } else if (line.find("off") != std::string::npos) {
+        location.is_cgi_ = false;
+      } else {
+        std::cerr << "invalid conf file" << std::endl;
+        throw std::invalid_argument("invalid conf file");
+      }
+
+    } else if (line.find("cgi_executor ") != std::string::npos) {
+      location.cgi_executor_ = line.substr(line.find("cgi_executor ") + 13);
+      location.cgi_executor_.pop_back();
+
+    } else if (line.find("error_page ") != std::string::npos) {
+      std::string error_code =
+          line.substr(line.find("error_page ") + 11,
+                      line.rfind(" ") - (line.find("error_page ") + 11));
+      std::string error_page = line.substr(line.rfind(" ") + 1);
+      error_page.pop_back();
+      while (error_code.length()) {
+        if (error_code.find(' ') != std::string::npos) {
+          std::string tmp = error_code.substr(0, error_code.find(' '));
+          location.error_pages_[atoi(tmp.c_str())] = error_page;
+          error_code = error_code.substr(error_code.find(' ') + 1);
+        } else {
+          location.error_pages_[atoi(error_code.c_str())] = error_page;
+          break;
+        }
+      }
+    } else if (line.find("autoindex ") != std::string::npos) {
+      if (line.find("on") != std::string::npos) {
+        location.autoindex_ = true;
+      } else if (line.find("off") != std::string::npos) {
+        location.autoindex_ = false;
+      } else {
+        std::cerr << "invalid conf file" << std::endl;
+        throw std::invalid_argument("invalid conf file");
+      }
+
+    } else if (line.find("return ") != std::string::npos) {
+      location.return_ = line.substr(line.find("return ") + 7);
+      location.return_.pop_back();
+
+    } else if (line.find("}") != std::string::npos) {
+      break;
+
+    } else {
+      std::cerr << "invalid conf file 172" << std::endl;
+      throw std::invalid_argument("invalid conf file");
+    }
+  }
+}
+
+void Conf::print_conf() {
+  std::cout << "server num: " << this->server_vec_.size() << std::endl;
+  for (size_t i = 0; i < this->server_vec_.size(); i++) {
+    std::cout << "server[" << i << "]" << std::endl;
+    std::cout << "  listen: " << this->server_vec_[i].listen_.sin_addr.s_addr
+              << std::endl;
+    std::cout << "  port: " << ntohs(this->server_vec_[i].listen_.sin_port)
+              << std::endl;
+    for (size_t j = 0; j < this->server_vec_[i].sv_names_.size(); j++) {
+      std::cout << "  server_name: " << this->server_vec_[i].sv_names_[j]
+                << std::endl;
+    }
+    std::cout << "  location num: " << this->server_vec_[i].locations_.size()
+              << std::endl;
+    for (size_t j = 0; j < this->server_vec_[i].locations_.size(); j++) {
+      std::cout << "  location[" << j << "]" << std::endl;
+      std::cout << "    match: " << this->server_vec_[i].locations_[j].match_
+                << std::endl;
+      std::cout << "    allow_method: "
+                << this->server_vec_[i].locations_[j].allow_method_
+                << std::endl;
+      std::cout << "    client_max_body_size: "
+                << this->server_vec_[i].locations_[j].client_max_body_size_
+                << std::endl;
+      std::cout << "    root: " << this->server_vec_[i].locations_[j].root_
+                << std::endl;
+      std::cout << "    index: " << this->server_vec_[i].locations_[j].index_
+                << std::endl;
+      std::cout << "    is_cgi: "
+                << (this->server_vec_[i].locations_[j].is_cgi_ ? "true"
+                                                               : "false")
+                << std::endl;
+      std::cout << "    cgi_executor: "
+                << this->server_vec_[i].locations_[j].cgi_executor_
+                << std::endl;
+
+      for (std::map<int, std::string>::iterator it =
+               this->server_vec_[i].locations_[j].error_pages_.begin();
+           it != this->server_vec_[i].locations_[j].error_pages_.end(); it++) {
+        std::cout << "    error_page: " << std::endl;
+        std::cout << "      " << it->first << ": " << it->second << std::endl;
+      }
+
+      std::cout << "    autoindex: "
+                << (this->server_vec_[i].locations_[j].autoindex_ ? "true"
+                                                                  : "false")
+                << std::endl;
+      std::cout << "    return: " << this->server_vec_[i].locations_[j].return_
+                << std::endl;
+    }
+  }
+}
+
+int main(int ac, char **ag) {
+  Conf conf(ag[1]);
+  conf.print_conf();
+}
 
 // デストラクタ
-conf::~conf() {}
+Conf::~Conf() {}
--- srcs/conf/conf.hpp
+++ srcs/conf/conf.hpp
@@ -3,7 +3,7 @@ webserverの設定を扱うクラス
 設定ファイルのパスを引数にとり、設定ファイルを読み込む
 設定ファイルが無い場合はデフォルトの内容を設定する
 設定ファイルのフォーマットは以下の通り
-・設定ファイルの拡張子は.conf
+・設定ファイルの拡張子は.Conf
 
 
 
@@ -16,16 +16,72 @@ webserverの設定を扱うクラス
 
 #ifndef CONF_HPP
 #define CONF_HPP
-#include <iostream>
+#include <map>
+#include <netinet/in.h>
+#include <set>
+#include <string>
+#include <sys/socket.h>
+#include <vector>
 
-class conf {
+// 複数指定不可の単一のみの設定項目で、複数指定された場合は最後の一つだけ保持する
+enum match_type {
+  prefix, // 前方一致
+  back,   // 後方一致
+};
+
+enum method {
+  GET = 1,
+  POST = 2,
+  DELETE = 4,
+};
+
+struct Location {
+  match_type match_; // 後方一致は、CGIの場合のみ使用可能
+  int allow_method_; // GET POST DELETE から１個以上指定 ビット演算で複数指定可
+  size_t client_max_body_size_; // 任意 単一 デフォルト１MB, 0は無制限 制限超え
+                                // 413 Request Entity Too Large
+                                // 制限されるのはボディ部分でヘッダーは含まない
+  std::string root_; // 必須 単一 相対パスの起点
+  std::string index_; // 任意 単一 ディレクトリへのアクセス時に返すファイル
+  bool is_cgi_; // デフォルトはfalse 一致する場合は、CGIとして処理する
+  std::string cgi_executor_; // execve(cgi_path, X, X) is_cgi_がtrueの場合必須
+  std::map<int, std::string> error_pages_;
+  // 任意 複数可 mapのkeyはエラーコード, valueはエラーページのパス
+  // エラーコードに重複があった場合は、最後の一つだけ保持する（上書きされる）
+  bool autoindex_;     // 任意 単一 デフォルトはfalse
+  std::string return_; // 任意 単一
+                       // HTTPステータスコード301と共に<path>へリダイレクトする｡
+  // std::string upload_path_;
+};
+
+struct Server { // 各バーチャルサーバーの設定を格納する
+  struct sockaddr_in listen_; // 必須 単一
+
+  std::vector<std::string> sv_names_;
+  // 任意 単一 ディレクティブは一つで、複数指定された場合は最後の一つだけ保持
+  // 一つのディレクティブ内に、サーバーネームは並べて複数可能
+
+  std::vector<Location> locations_; // 任意 複数可
+};
+
+class Conf {
 private:
-  conf(const conf &other); // コピーコンストラクタは不使用(シングルトン)
-  conf &operator=(const conf &other); // コピー代入演算子も不使用(シングルトン)
+  std::vector<Server> server_vec_; // 必須 複数可 複数の場合、一番上がデフォ
+
 public:
-  conf(); // デフォルトコンストラクタはコンフファイルが指定されなかった場合に使用
-  conf(const std::string &conf_path); // コンフファイルが指定された場合に使用
-  ~conf();                            // デストラクタ
+  Conf(const char *conf_path);
+  ~Conf();
+
+  void print_conf(); // デバッグ用
+
+private:
+  void parse_sever(std::ifstream &ifs, Server &server);
+  void parse_location(std::ifstream &ifs, Location &location);
+
+  // 不使用だが、コンパイラが自動生成し、予期せず使用するのを防ぐために記述
+  Conf(const Conf &other); // コピーコンストラクタは不使用(シングルトン)
+  Conf &operator=(const Conf &other); // コピー代入演算子も不使用(シングルトン)
+  Conf();
 };
 
 #endif
